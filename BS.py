da2ca114263d3ce6d4beb43e1315049bceb93631
Nathaniel <zhac207@live.rhul.ac.uk>
2024-01-28 12:22:10 +0000

    Update BS.py

--- BS.py
+++ BS.py
@@ -1,3 +1,5 @@
+
+
 grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 for i in range(10):
     print(grid[i])
@@ -28,7 +30,7 @@ def chooseShipPos():
         elif (n == 2) or (n == 3):
             ship_Size = 3
         elif n == 4:
-            ship_Size = 3
+            ship_Size = 2
         print("Enter row number (0-9) for the start of " + i + " (" + str(ship_Size) + " spaces): ")
         x_Row = int(input())
         print("Enter column number (0-9) for the start of " + i + " (" + str(ship_Size) + " spaces): ")
@@ -37,14 +39,15 @@ def chooseShipPos():
         x_Orientation = input()
 
 
+
         if n == 0:
             grid[x_Row][x_Col] = 1
             blaharier_Pos = [x_Row, x_Col]
-            if x_Orientation == "H":
+            if x_Orientation == "H" or x_Orientation == "h":
                 for i in range(blaharier_Num - 1):
                     x_Col = x_Col + 1
                     grid[x_Row][x_Col] = 1
-            elif x_Orientation == "V":
+            elif x_Orientation == "V" or x_Orientation == "v":
                 for i in range(blaharier_Num - 1):
                     x_Row = x_Row + 1
                     grid[x_Row][x_Col] = 1
@@ -53,11 +56,11 @@ def chooseShipPos():
         elif n == 1:
             battleblahaj_Pos = [x_Row, x_Col]
             grid[x_Row][x_Col] = 2
-            if x_Orientation == "H":
+            if x_Orientation == "H" or x_Orientation == "h":
                 for i in range(battleblahaj_Num - 1):
                     x_Col = x_Col + 1
                     grid[x_Row][x_Col] = 2
-            elif x_Orientation == "V":
+            elif x_Orientation == "V" or x_Orientation == "v":
                 for i in range(battleblahaj_Num - 1):
                     x_Row = x_Row + 1
                     grid[x_Row][x_Col] = 2
@@ -66,11 +69,11 @@ def chooseShipPos():
         elif n == 2:
             bluiser_Pos = [x_Row, x_Col]
             grid[x_Row][x_Col] = 3
-            if x_Orientation == "H":
+            if x_Orientation == "H" or x_Orientation == "h":
                 for i in range(bluiser_Num - 1):
                     x_Col = x_Col + 1
                     grid[x_Row][x_Col] = 3
-            elif x_Orientation == "V":
+            elif x_Orientation == "V" or x_Orientation == "v":
                 for i in range(blaharier_Num - 1):
                     x_Row = x_Row + 1
                     grid[x_Row][x_Col] = 3
@@ -79,11 +82,11 @@ def chooseShipPos():
         elif n == 3:
             sharkmarine_Pos = [x_Row, x_Col]
             grid[x_Row][x_Col] = 4
-            if x_Orientation == "H":
+            if x_Orientation == "H" or x_Orientation == "h":
                 for i in range(sharkmarine_Num - 1):
                     x_Col = x_Col + 1
                     grid[x_Row][x_Col] = 4
-            elif x_Orientation == "V":
+            elif x_Orientation == "V" or x_Orientation == "v":
                 for i in range(sharkmarine_Num - 1):
                     x_Row = x_Row + 1
                     grid[x_Row][x_Col] = 4
@@ -92,11 +95,11 @@ def chooseShipPos():
         elif n == 4:
             desharker_Pos = [x_Row, x_Col]
             grid[x_Row][x_Col] = 5
-            if x_Orientation == "H":
+            if x_Orientation == "H" or x_Orientation == "h":
                 for i in range(desharker_Num - 1):
                     x_Col = x_Col + 1
                     grid[x_Row][x_Col] = 5
-            elif x_Orientation == "V":
+            elif x_Orientation == "V" or x_Orientation == "v":
                 for i in range(desharker_Num - 1):
                     x_Row = x_Row + 1
                     grid[x_Row][x_Col] = 5
@@ -105,9 +108,8 @@ def chooseShipPos():
         n = n + 1
     for i in range(100):
         print()
-    
 
-def hitOrMiss():
+def initHitMiss():
     blaharier_Left = 5
     battleblahaj_Left = 4
     bluiser_Left = 3
@@ -118,6 +120,9 @@ def hitOrMiss():
     bluiser_Sunk = False
     sharkmarine_Sunk = False
     desharker_Sunk = False
+    return (blaharier_Left, battleblahaj_Left, bluiser_Left, sharkmarine_Left, desharker_Left, blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk)
+    
+def hitOrMiss(blaharier_Left, battleblahaj_Left, bluiser_Left, sharkmarine_Left, desharker_Left, blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk):
     print("Enter row number (0-9) to attack: ")
     hit_Row = int(input())
     print("Enter column number (0-9) to attack: ")
@@ -133,7 +138,7 @@ def hitOrMiss():
                     print("BLAHARIER SUNK")
                     blaharier_Sunk = True
                 grid[hit_Row][hit_Col] = 9
-            if (grid[hit_Row][hit_Col] == 2) and (hasHit == False):
+            elif (grid[hit_Row][hit_Col] == 2) and (hasHit == False):
                 hasHit = True
                 print("HIT")
                 battleblahaj_Left -= 1
@@ -141,7 +146,7 @@ def hitOrMiss():
                     print("BATTLEBLAHAJ SUNK")
                     battleblahaj_Sunk = True
                 grid[hit_Row][hit_Col] = 9
-            if (grid[hit_Row][hit_Col] == 3) and (hasHit == False):
+            elif (grid[hit_Row][hit_Col] == 3) and (hasHit == False):
                 hasHit = True
                 print("HIT")
                 bluiser_Left -= 1
@@ -149,7 +154,7 @@ def hitOrMiss():
                     print("BLUSIER SUNK")
                     bluiser_Sunk = True
                 grid[hit_Row][hit_Col] = 9
-            if (grid[hit_Row][hit_Col] == 4) and (hasHit == False):
+            elif (grid[hit_Row][hit_Col] == 4) and (hasHit == False):
                 hasHit = True
                 print("HIT")
                 sharkmarine_Left -= 1
@@ -157,25 +162,32 @@ def hitOrMiss():
                     print("SHARKMARINE SUNK")
                     sharkmarine_Sunk = True
                 grid[hit_Row][hit_Col] = 9
-            if (grid[hit_Row][hit_Col] == 5) and (hasHit == False):
+            elif (grid[hit_Row][hit_Col] == 5) and (hasHit == False):
                 hasHit = True
                 print("HIT")
                 desharker_Left -= 1
-                if desharker_Left <= 0:
+                if desharker_Left == 0:
                     print("DESHARKER SUNK")
                     desharker_Sunk = True
-                grid[hit_Row][hit_Col] = 9            
-            else:
-                print("MISS")
-    win(blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk)    
-
+                grid[hit_Row][hit_Col] = 9         
+    if hasHit == False:
+        print("MISS")
+    print()
+    win(blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk) 
+    return (blaharier_Left, battleblahaj_Left, bluiser_Left, sharkmarine_Left, desharker_Left, blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk)
+   
 def win(blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk):
     if (blaharier_Sunk == True) and (battleblahaj_Sunk == True) and (bluiser_Sunk == True) and (sharkmarine_Sunk == True) and (desharker_Sunk == True):
         hasWon = True
         print("You win!")
         print("All ships are sunk!")
-
+        
+        
 
 chooseShipPos()
+firstGo = True
+blaharier_Left, battleblahaj_Left, bluiser_Left, sharkmarine_Left, desharker_Left, blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk = initHitMiss()
 while (not hasWon):
-    hitOrMiss()
+    blaharier_Left, battleblahaj_Left, bluiser_Left, sharkmarine_Left, desharker_Left, blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk = hitOrMiss(blaharier_Left, battleblahaj_Left, bluiser_Left, sharkmarine_Left, desharker_Left, blaharier_Sunk, battleblahaj_Sunk, bluiser_Sunk, sharkmarine_Sunk, desharker_Sunk)
+    if hasWon == True:
+        break
